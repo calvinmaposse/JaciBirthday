7e36387c49866a0a017a8460ad54318fae5dfd51
calvinmaposse <111870297+calvinmaposse@users.noreply.github.com>
2023-04-15 00:53:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const reasonsContainer = document.getElementById("reasons-container");
 // Generate the reason buttons
 for (let i = 0; i <= 23; i++) {
   // If the current hour is greater than the button number, show the button
-  //if(true){
-  if (i <= currentHour) {
+  if(true){
+  //if (i <= currentHour) {
     const reasonButton = document.createElement("button");
     reasonButton.classList.add("reason-button");
     reasonButton.innerHTML = '<button class="reason-button" id="reason-'+ i + '"'+ '>Motivo #'+ i + '</button>';
